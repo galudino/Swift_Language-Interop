010f4170aed5591b269fd70a8fdbc2fdf3eda8fb
Gemuele Aludino <g.aludino@gmail.com>
2021-05-13 18:29:53 -0400

    Clean up main.swift for Objective-C++ example

--- StackTest-ObjCPP/main.swift
+++ StackTest-ObjCPP/main.swift
@@ -8,7 +8,7 @@
 func useObjCPPStack() {
     // Declare a StackInt
     // Initialize the stack to a capacity of 1
-    let s: StackInt = StackInt(10)
+    let s: StackInt = StackInt(1)
     
     // Push integers 10, 9, 8 ... 1 to the stack
     for i: Int32 in (1...10).reversed() {
@@ -23,12 +23,6 @@ func useObjCPPStack() {
         
         s.pop()
     }
-    
-    s.deinit()
-    
-    s.push(1123)
-    
-    print("top after deinit: \(s.top())")
 }
 
 useObjCPPStack()
